c1515ba3993fcf9007a9c5e053dae65f2eb219a2
Dastan Yessekeyev <21.nov.1988@gmail.com>
2020-10-06 10:18:19 +0600

    toastr style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ToastrService} from 'ngx-toastr';
 import {HttpClient} from '@angular/common/http';
 
@@ -7,7 +7,7 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   playerId: any;
   sizeX: any;
   sizeY: any;
@@ -16,8 +16,12 @@ export class AppComponent {
               private toastr: ToastrService) {
   }
 
+  ngOnInit(): void {
+    this.http.get<string>('/api').subscribe(res => this.toastr.info(res));
+  }
+
   create() {
-    this.http.post('locahost:8080/player', { id: this.playerId, size: { x: this.sizeX, y: this.sizeY }})
+    this.http.post('/api/player', { id: this.playerId, size: { x: this.sizeX, y: this.sizeY }})
       .subscribe(
         data => this.toastr.success('Player created'),
         error => this.toastr.error(error)
