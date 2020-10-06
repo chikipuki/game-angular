447a26321dae78a1b82c8c9c916f67c7a601354b
Dastan Yessekeyev <21.nov.1988@gmail.com>
2020-10-06 11:25:53 +0600

    some more stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent implements OnInit {
   playerId: any;
   sizeX: any;
   sizeY: any;
+  position: any = { finished: '?', x: '?', y: '?' };
+  route: any = '?';
+  players: [];
 
   constructor(private http: HttpClient,
               private toastr: ToastrService) {
@@ -18,13 +21,66 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.get<string>('/api').subscribe(res => this.toastr.info(res));
+    this.getPlayers();
+  }
+
+  delete() {
+    this.http.delete(`/api/${this.playerId}`)
+      .subscribe(
+        data => this.toastr.success('Player deleted'),
+        error => {
+          this.toastr.error(error.error.message, error.error.status);
+        }
+      );
   }
 
   create() {
     this.http.post('/api/player', { id: this.playerId, size: { x: this.sizeX, y: this.sizeY }})
       .subscribe(
         data => this.toastr.success('Player created'),
-        error => this.toastr.error(error)
+        error => {
+          this.toastr.error(error.error.message, error.error.status);
+        }
+      );
+  }
+
+  move(direction: string) {
+    this.http.patch<any>(`/api/${this.playerId}/move?direction=${direction}`, null)
+      .subscribe(
+        data => this.toastr.success(data.success + ''),
+        error => {
+          this.toastr.error(error.error.message, error.error.status);
+        }
+      );
+  }
+
+  requestPosition() {
+    this.http.get<any>(`/api/${this.playerId}/position`)
+      .subscribe(
+        res => this.position = res,
+        error => {
+          this.toastr.error(error.error.message, error.error.status);
+        }
+      );
+  }
+
+  requestRoute() {
+    this.http.get<any>(`/api/${this.playerId}/route`)
+      .subscribe(
+        res => this.route = res,
+        error => {
+          this.toastr.error(error.error.message, error.error.status);
+        }
+      );
+  }
+
+  getPlayers() {
+    this.http.get<any>(`/api/players`)
+      .subscribe(
+        res => this.players = res.players,
+        error => {
+          this.toastr.error(error.error.message, error.error.status);
+        }
       );
   }
 }
