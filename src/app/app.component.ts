5b5ffd6802c389df1a3a48ae6c694178ce8a8bc1
Dastan Yessekeyev <21.nov.1988@gmail.com>
2020-10-06 10:05:41 +0600

    some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  playerId: string;
+  playerId: any;
+  sizeX: any;
+  sizeY: any;
+
+  constructor(private http: HttpClient,
+              private toastr: ToastrService) {
+  }
+
+  create() {
+    this.http.post('locahost:8080/player', { id: this.playerId, size: { x: this.sizeX, y: this.sizeY }})
+      .subscribe(
+        data => this.toastr.success('Player created'),
+        error => this.toastr.error(error)
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
